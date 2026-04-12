f09aa44a86af4d9965de3878590129bba112c951
DELL <DELL@SAMAR>
2026-04-12 11:04:44 +0300

    add fix migration

---
+++ ModernPaySystem.Infrastructure.Persistence/Migrations/20260412080256_AddResponseTransactionTable2.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ModernPaySystem.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddResponseTransactionTable2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ResponseTransactions_ResponseTransactions_ParentTransaction~",
+                table: "ResponseTransactions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ResponseTransactions_Users_CurrentUserHolderId",
+                table: "ResponseTransactions");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ResponseTransactions_ResponseTransactions_ParentTransaction~",
+                table: "ResponseTransactions",
+                column: "ParentTransactionId",
+                principalTable: "ResponseTransactions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ResponseTransactions_Users_CurrentUserHolderId",
+                table: "ResponseTransactions",
+                column: "CurrentUserHolderId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ResponseTransactions_ResponseTransactions_ParentTransaction~",
+                table: "ResponseTransactions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ResponseTransactions_Users_CurrentUserHolderId",
+                table: "ResponseTransactions");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ResponseTransactions_ResponseTransactions_ParentTransaction~",
+                table: "ResponseTransactions",
+                column: "ParentTransactionId",
+                principalTable: "ResponseTransactions",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ResponseTransactions_Users_CurrentUserHolderId",
+                table: "ResponseTransactions",
+                column: "CurrentUserHolderId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
