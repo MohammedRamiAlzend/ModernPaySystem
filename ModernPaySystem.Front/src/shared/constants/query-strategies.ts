96432930703e37565d0b32d7a5d6078acf245140
Mhd <engmhdmajid@gmail.com>
2026-04-13 16:27:22 -0700

    .

--- ModernPaySystem.Front/src/shared/constants/query-strategies.ts
+++ ModernPaySystem.Front/src/shared/constants/query-strategies.ts
@@ -52,10 +52,10 @@ export const QUERY_STRATEGIES: Record<UpdateStrategy, QueryConfigOptions> = {
     },
     [UpdateStrategy.LIVE]: {
         staleTime: 0,
-        gcTime: 1000 * 60 * 1200,
-        // gcTime: 1000 * 60 * 5,
+        gcTime: 1000 * 60 * 5,
         retry: 1,
         refetchOnWindowFocus: true,     // تحديث عند العودة
-        refetchInterval: 5000,          // جلب تلقائي كل 5 ثوانٍ
+        // refetchInterval: 5000,          // جلب تلقائي كل 5 ثوانٍ
+        refetchInterval: 500000000,          // جلب تلقائي كل 5 ثوانٍ
     },
 };
