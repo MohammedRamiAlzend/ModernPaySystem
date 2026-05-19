4caa58fae145379298cca499fdfbca67ace0f069
Mhd <engmhdmajid@gmail.com>
2026-05-18 17:05:24 -0700

    .

--- ModernPaySystem.Front/src/app/router/routes/components/RootRedirect.tsx
+++ ModernPaySystem.Front/src/app/router/routes/components/RootRedirect.tsx
@@ -1,11 +1,12 @@
 import { Navigate } from 'react-router-dom';
-import { useAuthStore } from '@/app/store/authStore';
+// import { useAuthStore } from '@/app/store/authStore';
 
 export const RootRedirect = () => {
-  const user = useAuthStore((state) => state.user);
-  const targetPath = user?.isDepartmentHead 
-    ? "/form-builder/responses" 
-    : "/form-builder/referrals/pending";
-  
+  // const user = useAuthStore((state) => state.user);
+  const targetPath = "form-builder/reports"
+  // const targetPath = user?.isDepartmentHead 
+  //   ? "/form-builder/responses" 
+  //   : "/form-builder/referrals/pending";
+
   return <Navigate to={targetPath} replace />;
 };
