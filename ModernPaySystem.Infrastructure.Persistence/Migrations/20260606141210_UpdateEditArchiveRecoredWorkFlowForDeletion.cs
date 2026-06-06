7a525efff6b1465c714eaa46b70a1be494fe147c
Rami <Dell@DESKTOP-J416FJ3>
2026-06-06 17:13:57 +0300

    add migration for archive recored deletion

---
+++ ModernPaySystem.Infrastructure.Persistence/Migrations/20260606141210_UpdateEditArchiveRecoredWorkFlowForDeletion.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ModernPaySystem.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEditArchiveRecoredWorkFlowForDeletion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RequestedFileDeletionIdsJson",
+                table: "EditArchiveRequests",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RequestedFileDeletionIdsJson",
+                table: "EditArchiveRequests");
+        }
+    }
+}
